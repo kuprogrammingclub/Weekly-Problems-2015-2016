dc65634e17ab771aa3fed1e334bbe741ecc9b650
bcaudell95 <bcaudell95@gmail.com>
2016-02-06 18:22:11 -0600

    Brandon's python solution to Week07/Intermediate1

---
+++ Week07_Feb10/Intermediate1/bcaudell/treeheight.py
@@ -0,0 +1,23 @@
+class Node:
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
+
+	def insert(self, value):
+		if value < self.value:
+			if self.left == None:
+				self.left = Node(value)
+			else:
+				self.left.insert(value)
+		else:
+			if self.right == None:
+				self.right = Node(value)
+			else:
+				self.right.insert(value)
+
+	def height(self):
+		leftheight = self.left.height() if self.left != None else 0
+		rightheight = self.right.height() if self.right != None else 0
+		return leftheight + 1 if leftheight > rightheight else rightheight + 1
+
